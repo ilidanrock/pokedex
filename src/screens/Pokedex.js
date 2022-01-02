ada74710840e4346635c18b56d7719937b852d27
Luis Rivera <rluis747@gmail.com>
2022-01-02 12:23:47 -0500

    Manejo de Listas y consumo de APIs

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, SafeAreaView } from "react-native";
+import { getPokemonsApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  useEffect(() => {
+    (async () => {
+      await LoadPokemons();
+    })();
+  }, []);
+
+  const LoadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
