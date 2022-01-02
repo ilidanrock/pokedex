c3903ce916eee19f64aa1b5330e6565ee4f9f47a
Luis Rivera <rluis747@gmail.com>
2022-01-02 17:32:16 -0500

    FlatList Pokemon

---
+++ src/components/PokemonList.js
@@ -0,0 +1,21 @@
+import { StyleSheet, FlatList, Text } from "react-native";
+
+export default function PokemonList(props) {
+  const { pokemons } = props;
+  console.log(pokemons);
+  return (
+    <FlatList
+      data={pokemons}
+      numColumns={2}
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(pokemon) => String(pokemon.id)}
+      renderItem={({ item }) => <Text>{item.name}</Text>}
+      contentContainerStyle={styles.flatListContentContainer}
+    />
+  );
+}
+const styles = StyleSheet.create({
+  flatListContentContainer: {
+    paddingHorizontal: 5,
+  },
+});
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Text, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 import { getPokemonsApi } from "../api/pokemon";
+import { getPokemonDetailsByUrlApi } from "../api/pokemon";
+import PokemonList from "../components/PokemonList";
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
   useEffect(() => {
     (async () => {
       await LoadPokemons();
@@ -11,15 +14,27 @@ export default function Pokedex() {
 
   const LoadPokemons = async () => {
     try {
+      const pokemonsArray = [];
       const response = await getPokemonsApi();
-      console.log(response);
+      for await (const pokemon of response.results) {
+        const pokemonsDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonsDetails.id,
+          name: pokemonsDetails.name,
+          type: pokemonsDetails.types[0].type.name,
+          order: pokemonsDetails.order,
+          imagen:
+            pokemonsDetails.sprites.other["official-artwork"].front_default,
+        });
+      }
+      setPokemons([...pokemons, ...pokemonsArray]);
     } catch (error) {
       console.error(error);
     }
   };
   return (
     <SafeAreaView>
-      <Text>Pokedex</Text>
+      <PokemonList pokemons={pokemons} />
     </SafeAreaView>
   );
 }
