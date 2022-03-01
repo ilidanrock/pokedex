63237421a50f46fedb44e11629eb3ec2aaf52008
Luis <rluis747@gmail.com>
2022-03-01 18:29:25 -0500

    Pokemons by type

---
+++ src/components/Pokemon/Type.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { map, capitalize } from "lodash";
+
+import getColorByPokemon from "../../utilis/getColorByPokemon";
+const Type = ({ types }) => {
+  return (
+    <View style={styles.content}>
+      {map(types, (item, index) => (
+        <View
+          key={index}
+          style={{
+            ...styles.pill,
+            backgroundColor: getColorByPokemon(item.type.name),
+          }}
+        >
+          <Text>{capitalize(item.type.name)}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+export default Type;
+
+const styles = StyleSheet.create({
+  content: {
+    marginTop: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pill: {
+    paddingHorizontal: 30,
+    paddingVertical: 5,
+    borderRadius: 20,
+    marginHorizontal: 10,
+    //backgroundColor: "#f0f"
+  },
+});
