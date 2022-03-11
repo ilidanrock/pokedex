f6e752adb28bad9ef6fe150e9bc738185b6a8adc
Luis <rluis747@gmail.com>
2022-03-11 16:34:34 -0500

    Maquetación del Pokemon: barra de navegación

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -4,15 +4,31 @@ import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
 import Stats from "../components/Pokemon/Stats";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { size } from "lodash";
 
 export default function Pokemon(props) {
   const {
-    navigation,
+    navigation, //navigation solo llega si es un screen si es un compoenete no llega
     route: { params },
   } = props;
-  console.log("ID", params.id);
 
   const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#fff"
+          size={20}
+          style={{ marginLeft: 10 }}
+          onPress={()=> navigation.goBack()}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
@@ -40,7 +56,7 @@ export default function Pokemon(props) {
         type={pokemon.types[0].type.name}
       />
       <Type types={pokemon.types} />
-      <Stats stats={pokemon.stats}/>
+      <Stats stats={pokemon.stats} />
     </ScrollView>
   );
 }
