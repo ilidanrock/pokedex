9efb8c72ce35e4157dc6abe4c9c0680ead0ad9c4
Luis <rluis747@gmail.com>
2022-03-16 19:22:07 -0500

    Navigation to back

--- src/navigation/FavoriteNavigation.js
+++ src/navigation/FavoriteNavigation.js
@@ -1,11 +1,20 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Favorite from "../screens/Favorite";
+import Pokemon from "../screens/Pokemon";
 
 const Stack = createNativeStackNavigator();
 export default function FavoriteNavigation() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="FavoriteScreen" component={Favorite} />
+      <Stack.Screen name="PokemonScreen" component={Pokemon} 
+        options={{
+          title: "",
+          headerShown: true,
+          headerTransparent: true,
+          headerShadowVisible: false,
+        }}
+      />
     </Stack.Navigator>
   );
 }
